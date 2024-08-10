babf6288b5179eb33fff68a781bf5185a8be67c6
AtomChidjai <atomchidjai@gmail.com>
2024-08-10 15:43:17 +0700

    add daisyui

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -3,15 +3,7 @@ import React from 'react'
 const LoginPage = () => {
   return (
     <>
-        <div className='w-[500px] h-[500px] bg-blue-100 mx-auto my-auto'>
-            <h1>Login Page</h1>
-            <form>
-                <label>Username</label>
-                <input type="text" />
-                <label>Password</label>
-                <input type="password" />
-            </form>
-        </div>
+        <h1>This is a loginPage</h1>
     </>
   )
 }
