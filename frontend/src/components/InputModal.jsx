fcd777375fa0916fb57ae3cf84af9e7d6fd89f82
AtomChidjai <atomchidjai@gmail.com>
2024-08-21 16:35:30 +0700

    fix modal css

--- frontend/src/components/InputModal.jsx
+++ frontend/src/components/InputModal.jsx
@@ -26,20 +26,27 @@ const InputModal = ({ InputModalToggle, closeInputModal, onUpdate, id }) => {
         <>
             {InputModalToggle &&
                 <div>
-                    <p className='z-50 fixed text-[30px] sm:text-[50px] hover:cursor-pointer text-white right-[10px] sm:right-[20px] top-[10px] sm:top-[20px]' onClick={closeInputModal}>❌</p>
-                    <div className='fixed inset-0 bg-black opacity-50 z-40'></div>
-                    <div className='w-[90%] sm:w-[800px] sm:h-[600px] bg-white fixed left-[50%] top-[50%] z-50 opacity-90 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl px-4 py-4 sm:px-2 '>
-                        <form onSubmit={TaskUpdateHandler}>
-                            <label>Updated Content</label>
-                            <input 
-                                type="text" 
-                                placeholder='Updated Content'
-                                value={upContent}
-                                onChange={ (e) => setUpContent(e.target.value) }
-                            />
-                        </form>
-                    </div>
+                <p
+                    className="z-50 fixed text-[30px] sm:text-[30px] cursor-pointer text-white right-4 sm:right-5 top-24"
+                    onClick={closeInputModal}
+                >
+                    ❌
+                </p>
+                <div className="fixed inset-0 bg-black opacity-50 z-40"></div>
+                <div className="fixed left-1/2 top-1/2 z-50 transform -translate-x-1/2 -translate-y-1/2 bg-white w-[90%] sm:w-[700px] sm:h-[400px] rounded-2xl p-4 sm:p-6">
+                    <h2 className="text-3xl sm:text-4xl font-bold text-center mb-6">Edit Task</h2>
+                    <form onSubmit={TaskUpdateHandler} className="flex flex-col items-center space-y-4">
+                        <textarea
+                            type="text"
+                            placeholder="Updated Content"
+                            value={upContent}
+                            onChange={(e) => setUpContent(e.target.value)}
+                            className="textarea w-full sm:w-[600px] w-[600px] p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 h-[200px] sm:w-[200px]"
+                        />
+                        <button className="btn btn-primary mt-4 w-[200px] text-[20px]">Edit</button>
+                    </form>
                 </div>
+            </div>
             }
         </>
     )
--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Modal = ({ modalToggle, closeModal, desc }) => {
     return (
         <>
-            {modalToggle &&
+            {modalToggle && (
                 <div>
-                    <p className='z-50 fixed text-[30px] sm:text-[50px] hover:cursor-pointer text-white right-[10px] sm:right-[20px] top-[10px] sm:top-[20px]' onClick={closeModal}>❌</p>
-                    <div className='fixed inset-0 bg-black opacity-50 z-40'></div>
-                    <div className='w-[90%] sm:w-[800px] sm:h-[600px] bg-white fixed left-[50%] top-[50%] z-50 opacity-90 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl px-4 py-4 sm:px-2 '>
-                        <div className='flex flex-col sm:flex-row w-full h-full'>
-                            {desc}
+                    <p
+                        className="z-50 fixed text-[30px] sm:text-[30px] cursor-pointer text-white right-4 sm:right-5 top-24"
+                        onClick={closeModal}
+                    >
+                        ❌
+                    </p>
+                    <div className="fixed inset-0 bg-black opacity-50 z-40"></div>
+                    <div className="fixed left-1/2 top-1/2 z-50 transform -translate-x-1/2 -translate-y-1/2 rounded-2xl p-4 sm:p-2 bg-white w-[400px] sm:w-[400px] sm:h-[400px] overflow-y-auto opacity-90">
+                        <h1 className="text-5xl font-bold pl-6 pt-6 text-blue-400">Todo</h1>
+                        <div className="text-center rounded-lg max-w-md">
+                            <p className="py-6 px-6">
+                                {desc}
+                            </p>
                         </div>
+
                     </div>
                 </div>
-            }
+            )}
         </>
-    )
-}
+    );
+};
 
-export default Modal
+export default Modal;
