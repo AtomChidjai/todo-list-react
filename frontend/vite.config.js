c5b496f1645863a4bdefb0a1bd64ebcff03a05df
AtomChidjai <atomchidjai@gmail.com>
2024-08-22 23:38:29 +0700

    add vercel.json to frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/auth': {
-        target: 'http://localhost:3000',
+        target: `${process.env.VITE_REACT_BACKEND_URL}`,
         changeOrigin: true,
         secure: false,
       },
