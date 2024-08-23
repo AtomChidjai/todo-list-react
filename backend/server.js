f1e523e4d4808d1cc7b66fcdd553e07380891f12
AtomChidjai <atomchidjai@gmail.com>
2024-08-23 14:05:55 +0700

    fix cors request method

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
 import express from 'express';
-import router from './routers/route.js'
+import router from './routers/route.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-// import cors from 'cors';
+import cors from 'cors';
+
 dotenv.config();
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.json());
-// app.use(cors());
 
 app.use('/auth', router);
 
@@ -21,12 +27,12 @@ app.get('/', (req, res) => {
 });
 
 mongoose.connect(process.env.MONGO_DB_CONNECTION)
-.then( () => {
-    console.log('MongoDB connected');
-})
-.catch( (err) => {
-    console.log(`MONGO ERROR : ${err}`)
-})
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch((err) => {
+        console.log(`MONGO ERROR : ${err}`);
+    });
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on PORT : ${process.env.PORT}`);
